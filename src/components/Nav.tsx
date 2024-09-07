1eb9c9fda23eca1ad7cae1da28a6696bafcad651
alexandre dnk <dincq.alexandre@gmail.com>
2024-09-07 15:00:41 +0200

    chore: Update .gitignore and add new files
    This commit updates the .gitignore file to include new files and directories: /products, /public/products, .env, and /prisma/dev.db. These changes ensure that these files and directories are ignored by Git.
    
    The commit also adds a new file, src/lib/utils.ts, which contains utility functions for the application.
    
    The purpose of this commit is to update the repository's ignore rules and add new utility functions.

---
+++ src/components/Nav.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { cn } from "@/lib/utils"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import { ComponentProps, ReactNode, use } from "react"
+
+export function Nav({children}: {children: ReactNode}) {
+  return <nav className="flex justify-center px-4 bg-primary text-primary-foreground">{children}</nav>
+
+}
+
+export function NavLink(props: Omit<ComponentProps<typeof Link>, "className">) {
+  const pathname = usePathname()
+  return <Link {...props} className={cn("p-4 hover:bg-secondary hover:text-secondary-foreground focus-visible:bg-secondary focus-visible:text-secondary-foreground", pathname === props.href && "bg-background text-foreground" )} />
+}
