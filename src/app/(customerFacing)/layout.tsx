4c8f3590d30f6e3d3682514bc429f6a6ca69a71b
alexandre dnk <dincq.alexandre@gmail.com>
2024-09-10 14:53:23 +0200

    Refactor layout component and add logo to navigation bar

--- src/app/(customerFacing)/layout.tsx
+++ src/app/(customerFacing)/layout.tsx
@@ -2,16 +2,21 @@ import { Nav, NavLink } from "@/components/Nav";
 
 export const dynamic = "force-dynamic";
 
+import Image from 'next/image';
+import logo from '../../assets/logo/logo.svg';
+
 export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return <>
-    <Nav>
-      <NavLink href="/">Dashboard</NavLink>
-      <NavLink href="/products">Products</NavLink>
-    </Nav>
+      <Nav>
+        <Image src={logo} width={40} height={40} alt="Logo" />
+        <h1 className="flex justify-center items-center mx-auto">Page Client</h1>
+        <NavLink href="/admin">Dashboard</NavLink>
+        <NavLink href="/admin/products">Products</NavLink>
+      </Nav>
     <div className="container my-6 ">{children}</div>
   </>
 }
