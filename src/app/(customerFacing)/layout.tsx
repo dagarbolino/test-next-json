953faeefcaed231b840ee2c67bb5b3e2fef928d7
alexandre dnk <dincq.alexandre@gmail.com>
2024-09-23 10:52:40 +0200

    Refactor layout component and update navigation bar links

--- src/app/(customerFacing)/layout.tsx
+++ src/app/(customerFacing)/layout.tsx
@@ -14,8 +14,8 @@ export default function Layout({
       <Nav>
         <Image src={logo} width={40} height={40} alt="Logo" />
         <h1 className="flex justify-center items-center mx-auto">Page Client</h1>
-        <NavLink href="/admin">Dashboard</NavLink>
-        <NavLink href="/admin/products">Products</NavLink>
+        <NavLink href="/">Dashboard</NavLink>
+        <NavLink href="/products">Products</NavLink>
       </Nav>
     <div className="container my-6 ">{children}</div>
   </>
